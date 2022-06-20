b024e0ea8f7a89b29962562779ea109247a76344
Violeta Pinto Leite <violetalopesalves@gmail.com>
2022-06-20 11:51:15 +0000

    Added new modals classes to the admin view

--- src/admin.py
+++ src/admin.py
@@ -14,3 +14,8 @@ def setup_admin(app):
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
+    admin.add_view(ModelView(Person, db.session))
+    admin.add_view(ModelView(Planet, db.session))
+    admin.add_view(ModelView(Favourites, db.session))
+
+
